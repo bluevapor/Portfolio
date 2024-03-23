2a32e08e41bac6f441db37dbfb69bba89429c8fb
bluevapor <160100304+bluevapor@users.noreply.github.com>
2024-03-23 16:10:35 -0400

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,18 +19,17 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I'm obsessed with progamming, game development, art, and writing!
               <br />
-              <br />I am fluent in classics like
+              <br />I can make sometimes functional things with
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> C++, Python and Go. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Test games, and artwork </b> and
                 also in areas related to{" "}
                 <b className="purple">
                   Blockchain.
@@ -38,12 +37,12 @@ function Home2() {
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
+              Whenever possible, I also apply my passion for art
               with <b className="purple">Node.js</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  my burning passion for game devlopment
                 </b>
               </i>
               &nbsp; like
