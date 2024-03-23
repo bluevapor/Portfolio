510ebf6532bb66112ce19a65b0d83aedb3d49dbf
bluevapor <160100304+bluevapor@users.noreply.github.com>
2024-03-23 16:11:19 -0400

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,9 +7,9 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Game Developer",
+          "Artist",
+          "WIP Author",
         ],
         autoStart: true,
         loop: true,
